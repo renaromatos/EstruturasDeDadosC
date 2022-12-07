e305c965817d94ba6f74e5fa6b42274fedd0cf0f
renaromatos <renaromatos@gmail.com>
2022-12-06 21:42:25 -0300

    Métodos de remoção.

--- Aula10/ListaSimplesmenteEncadeada.c
+++ Aula10/ListaSimplesmenteEncadeada.c
@@ -7,6 +7,48 @@ struct node{
 };
 typedef struct node Node;
 
+void removeTodos(Node *lista){
+
+    while(lista != NULL){
+        //guarda o próximo elemento
+        Node *proximo = lista->proximo;
+        //remove o elemento atual
+        free(lista);
+        //proximo elemento se torna o elemento atual
+        lista = proximo;
+    }
+    
+}
+
+Node* removeElemento(Node *lista, int valor){
+    //lista vazia
+    if(lista == NULL){
+        return lista;
+    }
+    //lista com valores            
+    else{
+        Node* anterior = NULL;
+        while(lista != NULL){
+            //elemento encontrado
+            if(lista->valor == valor){
+                if(anterior = NULL){
+                    //salvo o próximo elmento antes de desalocar
+                    Node *proximoAux = lista->proximo;
+                    free(lista);
+                    return proximoAux;
+                }else{
+                    //proximo elemento do anterior recebe o proximo elmento atual
+                    anterior->proximo = lista->proximo;
+                    free(lista);
+                    return lista;
+                }
+            }
+            anterior = lista;
+            lista = lista->proximo;
+        }
+    }
+}
+
 Node* insereFinal(Node *lista, int valor){
     if(lista == NULL){
         Node *node = (Node*)malloc(sizeof(Node));
@@ -55,14 +97,8 @@ int main(){
     Node* minhaLista = NULL;
     minhaLista = insereFinal(minhaLista, 100);
     minhaLista = insereFinal(minhaLista, 5);
-    
     minhaLista = insereInicio(minhaLista, 10);
     minhaLista = insereInicio(minhaLista, 1000);
-    
-    /**for(int i = 0; i < 100; i++){
-        minhaLista = insereInicio(minhaLista, rand()%1000);
-    }*/
-    
     imprimir(minhaLista);
 
 }
